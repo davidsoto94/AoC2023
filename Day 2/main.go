a491f081063eb6add07246445cbdf7d2c52de916
David Soto <davidsoto94@hotmail.com>
2023-12-14 17:51:43 -0500

    Part 1 completed

--- Day 2/main.go
+++ Day 2/main.go
@@ -19,13 +19,27 @@ func main() {
 	maxValues["red"] = 12
 	maxValues["green"] = 13
 	maxValues["blue"] = 14
+	total := 0
 	for scanner.Scan() {
 		games := strings.Split(scanner.Text(), ";")
-		gameValue, _ := strconv.ParseUint(strings.Split(strings.Split(games[0], ":")[0], " ")[1], 10, 0)
-		fmt.Println(gameValue)
+		gameValue, _ := strconv.Atoi(strings.Split(strings.Split(games[0], ":")[0], " ")[1])
 		games[0] = strings.Split(games[0], ":")[1]
+		impossible := false
 		for _, val := range games {
-			number := strings.Split(val, " ")[0]
+			values := strings.Split(val, ",")
+			for _, dices := range values {
+				key := strings.Split(dices, " ")[2]
+				number, _ := strconv.Atoi(strings.Split(dices, " ")[1])
+				if maxValues[key] < number {
+					impossible = true
+					break
+				}
+			}
+
+		}
+		if !impossible {
+			total += gameValue
 		}
 	}
+	fmt.Println(total)
 }
