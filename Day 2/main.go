1ab85197c658ec2f4091b9c04b4c39bf72e89633
David Soto <davidsoto94@hotmail.com>
2023-12-14 18:01:49 -0500

    Second day part 2

--- Day 2/main.go
+++ Day 2/main.go
@@ -16,30 +16,25 @@ func main() {
 	}
 	scanner := bufio.NewScanner(file)
 	maxValues := make(map[string]int)
-	maxValues["red"] = 12
-	maxValues["green"] = 13
-	maxValues["blue"] = 14
 	total := 0
 	for scanner.Scan() {
-		games := strings.Split(scanner.Text(), ";")
-		gameValue, _ := strconv.Atoi(strings.Split(strings.Split(games[0], ":")[0], " ")[1])
-		games[0] = strings.Split(games[0], ":")[1]
-		impossible := false
-		for _, val := range games {
-			values := strings.Split(val, ",")
-			for _, dices := range values {
+		maxValues["red"] = 1
+		maxValues["green"] = 1
+		maxValues["blue"] = 1
+		sets := strings.Split(scanner.Text(), ";")
+		sets[0] = strings.Split(sets[0], ":")[1]
+		for _, val := range sets {
+			colors := strings.Split(val, ",")
+			for _, dices := range colors {
 				key := strings.Split(dices, " ")[2]
 				number, _ := strconv.Atoi(strings.Split(dices, " ")[1])
 				if maxValues[key] < number {
-					impossible = true
-					break
+					maxValues[key] = number
 				}
 			}
-
 		}
-		if !impossible {
-			total += gameValue
-		}
+		gameValue := maxValues["blue"] * maxValues["green"] * maxValues["red"]
+		total += gameValue
 	}
 	fmt.Println(total)
 }
