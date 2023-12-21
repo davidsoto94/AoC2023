995c9c4dc4be9089464d0f3e5cf0ec69b9cbee8f
davidsotom94 <david.soto@appxcale.com>
2023-12-21 16:16:19 -0500

    c# day 5 TDD

---
+++ Day5/ConsoleApp/Part1/Part1Class.cs
@@ -0,0 +1,7 @@
+namespace Part1;
+public class Part1Class
+{
+    public int Part1Function(){
+        return 0;
+    }
+}
---
+++ Day5/Day5.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using Part1;
+
+namespace Day5.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        var part1 = new Part1Class();
+
+
+        var result = part1.Part1Function();
+
+        Assert.Equal(35, result);
+
+    }
+}
