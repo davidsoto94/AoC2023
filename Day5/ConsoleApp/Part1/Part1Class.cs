5d6a07386e372cb62dd50925baffa39ebf53ef4a
davidsotom94 <david.soto@appxcale.com>
2023-12-22 14:38:50 -0500

    Part 1 nailed

--- Day5/ConsoleApp/Part1/Part1Class.cs
+++ Day5/ConsoleApp/Part1/Part1Class.cs
@@ -33,10 +33,6 @@ public class Part1Class
             }
             part = line.Split(" ");
             if( part[0].Contains("seeds")){
-                for (long i = 1; i < part.Length;i++){
-                    //Console.WriteLine(part[i].Trim());
-                    Seeds.Add(long.Parse(part[i].Trim()));
-                }
                 continue;
             }
             if (!long.TryParse(part[0].Trim(),out long val)){
@@ -71,19 +67,35 @@ public class Part1Class
 
             //Console.WriteLine(selectedDictionary);
         }
-        Console.WriteLine(string.Join(" ", SeedToSoil.SelectMany(innerList => innerList)));
+        //Console.WriteLine(string.Join(" ", FertilizerToWater.SelectMany(innerList => innerList)));
         long soil=1000000000000000000;
         foreach( long seed in Seeds){
-            long result = soil;
-            foreach (List<long> line in SeedToSoil)
-            {
-                if ()
+            long result = seed;
+            result = GetMappedValue(result, SeedToSoil);
+            result = GetMappedValue(result, SoilToFertilizer);
+            result = GetMappedValue(result, FertilizerToWater);
+            result = GetMappedValue(result, WaterToLight);
+            result = GetMappedValue(result, LightToTemperature);
+            result = GetMappedValue(result, TemperatureToHumidity);
+            result = GetMappedValue(result, HumidityToLocation);
+            if(result<soil){
+                soil = result;
             }
+            
         }
-        
         return soil;
     }
 
+    public long GetMappedValue(long InsertValue, List<List<long>> Map){
+        long tempValue = InsertValue;
+        foreach (List<long> line in Map)
+        {
+            if (InsertValue >= line[1] && InsertValue < line[1]+line[2]){
+                tempValue = line[0] + (InsertValue-line[1]);
+            }
+        }
+        return tempValue;
+    }
 
     public void AddToDict(string[] line, List<List<long>> list){
         list.Add(new List<long>(){
